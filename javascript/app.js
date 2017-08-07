bfaf123adf2b6b0183541f9d2d4882935d9648eb
krystin villeneuve <krystinv@gmail.com>
2017-08-07 16:14:02 -0600

    implement addBook, removeBookByTitle, removeBookByAuthor functions

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,31 @@
-var Library = function () {};
+var Library = function () {
 
-var myBookArray = []; // = new Array();
+};
+
+Library.prototype.myBookArray = []; // = new Array();
 
 
+// add books to the library array
 Library.prototype.addBook = function(book){
-  myBookArray.push(book);
+  gLib.myBookArray.push(book);
 };
 
 Library.prototype.removeBook = function (book) {
-
+  for(var i = 0; i < gLib.myBookArray.length; i++) {
+    if (book == gLib.myBookArray[i]) {
+      gLib.myBookArray.splice(i, 1);
+    };
+  };
 };
 
+Library.prototype.removeBookByAuthor = function(authorName) {
+  for (var k in gLib.myBookArray) {
+    if (authorName == gLib.myBookArray[k].author) {
+      gLib.myBookArray.splice(k, 1);
+    };
+  };
+};
 
-//example: oArgs = {title: "It", author: "Stephen King", numpages = 390, pubDate: "03/12/1987"}
 var Book = function(oArgs) {
   this.title = oArgs.title;
   this.author = oArgs.author;
@@ -20,8 +33,8 @@ var Book = function(oArgs) {
   this.aDate = oArgs.date
 };
 
-var gBookOnegitignore_global = new Book(oArgs = {title: "It", author: "Stephen King", numpages: 390, pubDate: "03/12/1987"})
-
+var gBookOne = new Book(oArgs = {title: "It", author: "Stephen King", numpages: 390, pubDate: "03/12/1987"})
+var gBookTwo = new Book(oArgs = {title: "Slaughterhouse Five", author: "Kurt Vonnegut", numpages: 400, pubDate: "03/01/1969"})
 
 // same as var window = x;  g is for global
 window.gLib = new Library();
