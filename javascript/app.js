0f2a8a9592e156f9fd089ea9a4d423cb8173a36c
krystin villeneuve <krystinv@gmail.com>
2017-08-08 14:41:08 -0600

    add booleans to add/remove functions

--- javascript/app.js
+++ javascript/app.js
@@ -4,27 +4,37 @@ var Library = function () {
 
 Library.prototype.myBookArray = []; // = new Array();
 
-
 // add books to the library array
 Library.prototype.addBook = function(book){
-  gLib.myBookArray.push(book);
+  for (var i = 0; i < gLib.myBookArray.length; i++) {
+    if (gLib.myBookArray[i].title === book.title) {
+      return false;
+    }
+  }
+    gLib.myBookArray.push(book);
+    return true;
 };
 
 // fix this to remove by title instead of iterator
-Library.prototype.removeBookByTitle = function (book) {
-  for(var k in gLib.myBookArray) {
-    if (book == gLib.myBookArray[k].title) {
-      gLib.myBookArray.splice(k, 1);
-    };
-  };
+Library.prototype.removeBookByTitle = function (title) {
+  for (var k in gLib.myBookArray) {
+    if (!gLib.myBookArray[k].title == title) {
+      return false;
+    }
+  }
+  gLib.myBookArray.splice(k, 1);
+  return true;
 };
 
-Library.prototype.removeBookByAuthor = function(authorName) {
+Library.prototype.removeBookByAuthor = function (name) {
   for (var k in gLib.myBookArray) {
-    if (authorName == gLib.myBookArray[k].author) {
-      gLib.myBookArray.splice(k, 1);
-    };
-  };
+    if (gLib.myBookArray[k].author !== name) {
+      return false;
+    }
+  }
+
+  gLib.myBookArray.splice(k, 1);
+  return true;
 };
 
 var Book = function(oArgs) {
@@ -36,8 +46,5 @@ var Book = function(oArgs) {
 
 var gBookOne = new Book(oArgs = {title: "It", author: "Stephen King", numpages: 390, pubDate: "03/12/1987"})
 var gBookTwo = new Book(oArgs = {title: "Slaughterhouse Five", author: "Kurt Vonnegut", numpages: 400, pubDate: "03/01/1969"})
-var title = "Slaughterhouse Five";
-var author = "Kurt Vonnegut";
 
-// same as var window = x;  g is for global
 window.gLib = new Library();
