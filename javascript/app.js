ef14364549e799d5a9b7f8aabe755e7f8f15e3a1
krystin villeneuve <krystinv@gmail.com>
2017-08-08 16:30:03 -0600

    fix bugs with removeBookByAuthor and removeBookByTitle

--- javascript/app.js
+++ javascript/app.js
@@ -14,46 +14,33 @@ Library.prototype.addBook = function(book){
 };
 
 Library.prototype.removeBookByTitle = function (title) {
-	for (var i in gLib.myBookArray) {
-		if (gLib.myBookArray[i].title === title) {
-			gLib.myBookArray.splice(i, 1);
+	for (var k in gLib.myBookArray) {
+		if (gLib.myBookArray[k].title.toLowerCase() === title.toLowerCase()) {
+			gLib.myBookArray.splice(k, 1);
 			return true;
 		}
 	}
 	return false;
 };
 
-// fix this to remove by title instead of iterator
-// Library.prototype.removeBookByTitle = function (title) {
-//
-// 	var bool = false;
-// 	for (var i = 0; i < gLib.myBookArray.length; i++) {
-// 		if (gLib.myBookArray[i].title == title) {
-// 			bool = true;
-// 			gLib.myBookArray.splice(i, 1);
-// 		}
-// 	}
-// 	return bool;
-// };
-//
-// Library.prototype.removeBookByAuthor = function (authorName) {
-// 	for (var i = 0; i < gLib.myBookArray.length; i++) {
-// 		if (gLib.myBookArray[i].author !== authorName) {
-// 			return false;
-// 		}
-// 	}
-// 	gLib.myBookArray.splice(i, 1);
-// 	return true;
-// };
+Library.prototype.removeBookByAuthor = function (authorName) {
+	for (var k in gLib.myBookArray) {
+		if (gLib.myBookArray[k].author.toLowerCase() === authorName.toLowerCase()) {
+			gLib.myBookArray.splice(k, 1);
+			return true;
+		}
+	}
+	return false;
+};
 
-// Library.prototype.getRandomBook = function () {
-// 	for (var k in gLib.myBookArray) {
-// 		if (gLib.myBookArray.indexOf) {
-// 			return gLib.myBookArray[Math.floor(Math.random() * gLib.myBookArray.length)];
-// 		}
-// 	}
-// 	return null;
-// };
+Library.prototype.getRandomBook = function () {
+	for (var k in gLib.myBookArray) {
+		if (gLib.myBookArray.indexOf) {
+			return gLib.myBookArray[Math.floor(Math.random() * gLib.myBookArray.length)];
+		}
+	}
+	return null;
+};
 
 // Library.prototype.getBookByTitle = function (title) {
 // 	var temp = [];
