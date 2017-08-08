a8111a1656402ad0b633f4d1735fd8d931332d7a
krystin villeneuve <krystinv@gmail.com>
2017-08-08 14:59:12 -0600

    add eslint for style

--- javascript/app.js
+++ javascript/app.js
@@ -1,59 +1,59 @@
-var Library = function () {
-
-};
+var Library = function () {};
 
 Library.prototype.myBookArray = []; // = new Array();
 
 // add books to the library array
 Library.prototype.addBook = function(book){
-  for (var i = 0; i < gLib.myBookArray.length; i++) {
-    if (gLib.myBookArray[i].title === book.title) {
-      return false;
-    }
-  }
-    gLib.myBookArray.push(book);
-    return true;
+	for (var i = 0; i < gLib.myBookArray.length; i++) {
+		if (gLib.myBookArray[i].title === book.title) {
+			return false;
+		}
+	}
+	gLib.myBookArray.push(book);
+	return true;
 };
 
 // fix this to remove by title instead of iterator
 Library.prototype.removeBookByTitle = function (title) {
-  for (var k in gLib.myBookArray) {
-    if (!gLib.myBookArray[k].title == title) {
-      return false;
-    }
-  }
-  gLib.myBookArray.splice(k, 1);
-  return true;
+	for (var k in gLib.myBookArray) {
+		if (!gLib.myBookArray[k].title == title) {
+			return false;
+		}
+	}
+	gLib.myBookArray.splice(k, 1);
+	return true;
 };
 
 Library.prototype.removeBookByAuthor = function (name) {
-  for (var k in gLib.myBookArray) {
-    if (gLib.myBookArray[k].author !== name) {
-      return false;
-    }
-  }
+	for (var k in gLib.myBookArray) {
+		if (gLib.myBookArray[k].author !== name) {
+			return false;
+		}
+	}
 
-  gLib.myBookArray.splice(k, 1);
-  return true;
+	gLib.myBookArray.splice(k, 1);
+	return true;
 };
 
 Library.prototype.getRandomBook = function () {
-  for (var k in gLib.myBookArray) {
-    if (gLib.myBookArray.indexOf) {
-      return gLib.myBookArray[Math.floor(Math.random() * gLib.myBookArray.length)]
-    }
-  }
-    return null;
-}
-
-var Book = function(oArgs) {
-  this.title = oArgs.title;
-  this.author = oArgs.author;
-  this.numPages = oArgs.numPages;
-  this.aDate = oArgs.date
+	for (var k in gLib.myBookArray) {
+		if (gLib.myBookArray.indexOf) {
+			return gLib.myBookArray[Math.floor(Math.random() * gLib.myBookArray.length)];
+		}
+	}
+	return null;
 };
 
-var gBookOne = new Book(oArgs = {title: "It", author: "Stephen King", numpages: 390, pubDate: "03/12/1987"})
-var gBookTwo = new Book(oArgs = {title: "Slaughterhouse Five", author: "Kurt Vonnegut", numpages: 400, pubDate: "03/01/1969"})
+
+
+var Book = function(oArgs) {
+	this.title = oArgs.title;
+	this.author = oArgs.author;
+	this.numPages = oArgs.numPages;
+	this.aDate = oArgs.date;
+};
+
+var gBookOne = new Book(oArgs = {title: "It", author: "Stephen King", numpages: 390, pubDate: "03/12/1987"});
+var gBookTwo = new Book(oArgs = {title: "Slaughterhouse Five", author: "Kurt Vonnegut", numpages: 400, pubDate: "03/01/1969"});
 
 window.gLib = new Library();
