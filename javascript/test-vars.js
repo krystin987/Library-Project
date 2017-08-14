dc2b5532f00b5e52a8371817547f15aa7de475c5
krystin villeneuve <krystinv@gmail.com>
2017-08-13 21:10:15 -0600

    fix bug in getBooksByAuthor function and add more books for tests

--- javascript/test-vars.js
+++ javascript/test-vars.js
@@ -1,8 +1,44 @@
 // hardcoded variables exist for testing purposes
 var gBookOne = new Book(oArgs = {title: "It", author: "Stephen King", numPages: 390, pubDate: "03/12/1987"});
 var gBookTwo = new Book(oArgs = {title: "Slaughterhouse Five", author: "Kurt Vonnegut", numPages: 400, pubDate: "03/01/1969"});
+var gBookThree = new Book(oArgs = {title: "To Kill a Mockingbird", author: "Harper Lee", numPages: 281, pubDate: "07/11/1960"});
+var gBookFour = new Book(oArgs = {title: "Starship Troopers", author: "Robert A. Heinlein", numPages: 263, pubDate: "12/01/1959"});
+var gBookFive = new Book(oArgs = {title: "Candide", author: "Voltaire", numPages: 176, pubDate: "01/01/1759"});
 
 // hardcoded objects exist for test purposes
+var williamsonObj = [
+// Jack Williamson coined such phrases as: genetic engineering
+	new Book(oArgs =
+	{
+		title: "The Legion of Space",
+	 	author: "Jack Williamson",
+	 	numPages: 259,
+	 	pubDate: "1947"
+	}),
+
+	new Book(oArgs =
+	{
+		title: "The Cometeers",
+	 	author: "Jack Williamson",
+	 	numPages: 506,
+	 	pubDate: "1/1/1950"
+	}),
+	new Book(oArgs =
+	{
+		title: "Darker Than You Think",
+	 	author: "Jack Williamson",
+	 	numPages: 310,
+	 	pubDate: "1/1/1948"
+	}),
+	new Book(oArgs =
+	{
+		title: "Star Bridge",
+	 	author: "Jack Williamson",
+	 	numPages: 221,
+	 	pubDate: "1/1/1955"
+	})
+];
+
 var thompsonObj = [
 	new Book(oArgs =
 	{
@@ -17,6 +53,22 @@ var thompsonObj = [
 		author: "Hunter S. Thompson",
 		numPages: 624,
 		pubDate: "1/1/1979"
+	}),
+
+	new Book(oArgs =
+	{
+		title: "The Rum Diary",
+		author: "Hunter S. Thompson",
+		numPages: 204,
+		pubDate: "1/1/1998"
+	}),
+
+	new Book(oArgs =
+	{
+		title: "Hell's Angels: The Strange and Terrible Saga of the Outlaw Motorcycle Gangs",
+		author: "Hunter S. Thompson",
+		numPages: 278,
+		pubDate: "1/1/1967"
 	})
 ];
 
@@ -40,8 +92,12 @@ var knuthObj = [
 Library.prototype.addAllBooksTest = function() {
 	gLibDenver.addBook(gBookOne);
 	gLibDenver.addBook(gBookTwo);
+	gLibDenver.addBook(gBookThree);
+	gLibDenver.addBook(gBookFour);
+	gLibDenver.addBook(gBookFive);
 	gLibDenver.addBooks(knuthObj);
 	gLibDenver.addBooks(thompsonObj);
+	gLibDenver.addBooks(williamsonObj);
 	console.log(gLibDenver.myBookArray);
 };
 
